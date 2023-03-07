23ca766b338fe7c4785843fa705fd7ba1921e5b7
Medo <martinmederly@gmail.com>
2023-03-07 22:35:59 +0100

    database functions and basic json functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-mailing-list
 
 go 1.19
+
+require github.com/mattn/go-sqlite3 v1.14.16
---
+++ jsonapi/jsonapi.go
@@ -0,0 +1,46 @@
+package jsonapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func setJsonHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+}
+
+func fromJson[T any](body io.Reader, target T) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(body)
+	json.Unmarshal(buf.Bytes(), &target)
+}
+
+func returnJson[T any](w http.ResponseWriter, withData func() (T, error)) {
+	setJsonHeader(w)
+
+	data, serverErr := withData()
+
+	if serverErr != nil {
+		w.WriteHeader(500)
+		serverErrJson, err := json.Marshal(&serverErr)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		w.Write(serverErrJson)
+		return
+	}
+
+	dataJson, err := json.Marshal(&data)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Write(dataJson)
+
+}
